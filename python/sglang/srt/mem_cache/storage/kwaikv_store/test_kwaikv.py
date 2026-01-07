b801665e64ad8326647c7937b17fedec0f04d982
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-07 18:25:43 +0800

    add kwaikv test

---
+++ python/sglang/srt/mem_cache/storage/kwaikv_store/test_kwaikv.py
@@ -0,0 +1,9 @@
+from kwaikv_client import create_client, KwaiKVClient, KwaiKVClientError
+
+client = create_client(
+    host="127.0.0.1",
+    ksn="infra-kiwi-test",
+    cluster="testCluster",
+    chunk_size=5*1024*1024,  # 5MB
+    enable_checksum=False,
+)
