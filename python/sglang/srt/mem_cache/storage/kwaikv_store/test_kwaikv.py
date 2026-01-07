fb94a506fb5a1df331be2b3c402813b0f7aaafb8
赵威伦 <zhaoweilun@zhaoweiluns-MacBook-Pro.local>
2026-01-07 19:28:11 +0800

    test

--- python/sglang/srt/mem_cache/storage/kwaikv_store/test_kwaikv.py
+++ python/sglang/srt/mem_cache/storage/kwaikv_store/test_kwaikv.py
@@ -1,8 +1,7 @@
 from kwaikvclient import create_client, KwaiKVClient, KwaiKVClientError
 
 client = create_client(
-    host="10.82.231.23",
-    master_addr="172.28.116.157:10119",
+    host="127.0.0.1",
     ksn="infra-kiwi-test",
     cluster="testCluster",
     chunk_size=5*1024*1024,  # 5MB
